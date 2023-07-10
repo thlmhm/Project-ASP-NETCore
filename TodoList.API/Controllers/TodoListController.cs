de0411b56d0d6d829660bb4f7c8dff62dece59a3
thlmhm <lam.ht194786@sis.hust.edu.vn>
2023-07-10 08:49:37 +0700

    fix bug tạo mới xong sửa tự động duplicate

--- TodoList.API/Controllers/TodoListController.cs
+++ TodoList.API/Controllers/TodoListController.cs
@@ -8,24 +8,25 @@ using Shared.DTOs;
 
 namespace TodoList.API.Controllers
 {
-
+    //[Route("~/api/TodoList/[action]")]
+    [ApiController]
     public class TodoListController : BaseApiController
     {
         [HttpGet]
-
         public async Task<IActionResult> GetProducts()
         {
             return Ok(await Mediator.Send(new TodoQueries()));
 
         }
 
+        [Route("addtodolist")]
         [HttpPost]
-
         public async Task<IActionResult> CreateTodo([FromBody] TodoModel Model)
         {
             return Ok(await Mediator.Send(new CreateTodoCommand() { Model = Model }));
         }
 
+        [Route("UpdateToDoList")]
         [HttpPut]
         public async Task<IActionResult> ModifyTodo([FromBody] TodoModel Model)
         {
